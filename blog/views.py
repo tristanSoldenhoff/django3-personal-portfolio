eaa80dec7fbad2a9da9fd307356431fd4617ea76
tristan <tsoldenhoff@gmail.com>
2021-01-12 23:44:26 +0000

    First commit!

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
+
+def all_blogs(request):
+    blog_count = Blog.objects.count
+    blogs = Blog.objects.order_by('-date')[:3]      #makes sure that most recent blogs are on top of the html. The [:3] limits the number of blogs visible on the html page
+    return render(request, 'blog/all_blogs.html', {'blogs':blogs, 'blog_count':blog_count })
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog':blog})
